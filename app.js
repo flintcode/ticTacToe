8ff166e1f997bd8226c1cc0e5c21e7b324ee96c8
Flinton Yang <flintonyang@live.ca>
2021-01-24 16:10:47 -0800

    Updated gameboard and marker logic, and updated UI

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const wrapper = document.querySelector("#wrapper")
 
-const gameBoard = ((doc) => {
+const gameBoard = (() => {
     const boardArray = []
 
     const createCell = (value) => {
         const  square = document.createElement("div");
-        square.setAttribute("squareId", value);
+        square.id = value;
         square.classList.add("squares")
         wrapper.appendChild(square)  
         boardArray.push(square)
-    }
-
-    
+    }    
     return {createCell}
-})(document);
+})();
 
 gameBoard.createCell("s1")
 gameBoard.createCell("s2")
@@ -25,25 +23,70 @@ gameBoard.createCell("s7")
 gameBoard.createCell("s8")
 gameBoard.createCell("s9")
 
-const displayController = () => {
-    
-}
+const displayController = (() => {
+    const squareEach = document.getElementsByClassName('squares')
 
-const player = (marker) => {
-    
-    const writeToDom = (square) => {
-        square.addEventListener('click', (e) => {
-            e.target.textContent = marker;
+    //     const checkWin = (marker) => {
+    //         for (i = 0 ; i < squareEach.length; i++) {
+    //             squareEach[i].textContent.addEventListener('input', (e) => {
+    //                 if (document.getElementById("s1").textContent == marker && document.getElementById("s2").textContent == marker  && document.getElementById("s3").textContent == marker){
+    //                     alert ('winner!')
+    //                 }
+    //             })
+    //         }
+       
+    //  }      
+    const restart = (marker) => {
+        document.getElementById("restart").addEventListener('click', () => {
+            for (i = 0; i < squareEach.length; i++) {
+                squareEach[i].innerHTML = null;
+            }
         })
     }
-    return {writeToDom}
+
+    const writeToDom = (marker) => {
+
+        for (i = 0; i < squareEach.length; i++){
+            squareEach[i].addEventListener('click', (e) => {  
+                
+                    if ((document.getElementById("s1").textContent == marker && document.getElementById("s2").textContent == marker  && document.getElementById("s3").textContent == marker)||
+                    (document.getElementById("s4").textContent == marker && document.getElementById("s5").textContent == marker  && document.getElementById("s6").textContent == marker)|| 
+                    (document.getElementById("s7").textContent == marker && document.getElementById("s8").textContent == marker  && document.getElementById("s9").textContent == marker) || 
+                    (document.getElementById("s1").textContent == marker && document.getElementById("s4").textContent == marker  && document.getElementById("s7").textContent == marker)|| 
+                    (document.getElementById("s2").textContent == marker && document.getElementById("s5").textContent == marker  && document.getElementById("s8").textContent == marker) || 
+                    (document.getElementById("s3").textContent == marker && document.getElementById("s6").textContent == marker  && document.getElementById("s9").textContent == marker)||
+                    (document.getElementById("s1").textContent == marker && document.getElementById("s5").textContent == marker  && document.getElementById("s9").textContent == marker) ||
+                    (document.getElementById("s3").textContent == marker && document.getElementById("s5").textContent == marker  && document.getElementById("s7").textContent == marker)) {
+                        alert ('winner!') // works only if you click again
+                        restart(marker)
+                    }  
+
+                    wrapper.classList.toggle('active')
+                   if (e.target.textContent){
+                       alert('taken');
+                   }              
+                   else {
+                    return (wrapper.classList.contains('active')) ?  e.target.textContent = "X":e.target.textContent = "O";  
+                   }
+                   
+            })                  
+        }
+       
+    }
+
+     
+    return {writeToDom,restart}
+})();
+
+displayController.writeToDom("X")
+
+displayController.restart("X")
+
+
+const player = () => {
+    
+    
 }
 
-const p1 = player('X')
 
-p1.writeToDom(document)
-
-const p2 = player('O')
-
-p2.writeToDom(document)
 
